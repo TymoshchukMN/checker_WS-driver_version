0a75f5a3c63b56511a50640e9a3a8369a8220c1b
TymoshchukMN <TymoshchukMN@gmail.com>
2026-07-03 11:16:12 +0300

    added post

---
+++ API_processor/Mappers/WSwersionData.cs
@@ -0,0 +1,25 @@
+﻿namespace API_processor.Mappers
+{
+    public class WSwersionData
+    {
+        /// <summary>
+        /// Gets or sets a value indicating ComputerName.
+        /// </summary>
+        required public string ComputerName { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether if file exists.
+        /// </summary>
+        public bool ІsFileWxists { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value of file version.
+        /// </summary>
+        public string? FileVersion { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value of checking date.
+        /// </summary>
+        public string? CkeckDate { get; set; }
+    }
+}
--- API_processor/Program.cs
+++ API_processor/Program.cs
@@ -1,7 +1,12 @@
+using System.Text;
+using API_processor.Mappers;
+
 namespace API_processor
 {
     public class Program
     {
+        private static readonly SemaphoreSlim _semaphore = new (1, 1);
+
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
@@ -9,31 +14,40 @@ namespace API_processor
             // Add services to the container.
             builder.Services.AddAuthorization();
 
-
             var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
 
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
 
-            var summaries = new[]
+            app.MapPost("/add", async (WSwersionData rawData) =>
             {
-                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-            };
+                Console.WriteLine(rawData);
 
-            app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-            {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                    new WeatherForecast
-                    {
-                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                        TemperatureC = Random.Shared.Next(-20, 55),
-                        Summary = summaries[Random.Shared.Next(summaries.Length)]
-                    })
-                    .ToArray();
-                return forecast;
+                var uploadDirectory = Path.Combine(Directory.GetCurrentDirectory(), "Data");
+
+                Directory.CreateDirectory(uploadDirectory);
+
+                var filePath = Path.Combine(uploadDirectory, "DataFromPCs.txt");
+
+                StringBuilder tempString = new ();
+                tempString.Append($"{rawData.ComputerName};");
+                tempString.Append($"{rawData.CkeckDate};");
+                tempString.Append($"{rawData.²sFileWxists};");
+                tempString.AppendLine($"{rawData.FileVersion};");
+
+                await _semaphore.WaitAsync();
+
+                try
+                {
+                    await File.AppendAllTextAsync(filePath, tempString.ToString());
+                }
+                finally
+                {
+                    _semaphore.Release();
+                }
+
+                Console.WriteLine("done");
             });
 
             app.Run();
