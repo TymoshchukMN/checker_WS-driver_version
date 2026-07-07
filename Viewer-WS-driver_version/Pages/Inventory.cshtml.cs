2186ae318042e2eef5316ff04d5a3c868a94ba32
TymoshchukMN <TymoshchukMN@gmail.com>
2026-07-07 16:11:03 +0300

    added sidebar

---
+++ Viewer-WS-driver_version/Pages/Inventory.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Viewer_WS_driver_version.Pages
+{
+    public class InventoryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
